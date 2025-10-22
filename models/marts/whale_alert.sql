00f61607ea857f9cdb57c41de66f159adcf302b1
dilip parmra <dilippar@gmail.com>
2025-10-22 23:18:17 +0530

    dbt see

--- models/marts/whale_alert.sql
+++ models/marts/whale_alert.sql
@@ -1,3 +1,5 @@
+WITH WHALES as (
+
 select
 output_address,
 sum(output_value) as total_sent,
@@ -9,3 +11,19 @@ where output_value > 10
 
 group by output_address
 order by total_sent desc
+),
+
+LATEST_PRICE as (
+	select
+	price
+	from {{ ref('btc_usd_max')}}
+	where to_date(replace(snapped_at,' UTC','')) = current_date()
+)
+
+select
+w.output_address,
+w.total_sent,
+w.tx_count,
+(p.price * w.total_sent) as total_sent_usd
+from WHALES w
+cross join LATEST_PRICE p
