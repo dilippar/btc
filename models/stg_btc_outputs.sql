221b24e102712168ba5a92c4a9e8f3af8125dfbf
dilip parmra <dilippar@gmail.com>
2025-10-22 22:51:27 +0530

    ephemeral changes 18

--- models/stg_btc_outputs.sql
+++ models/stg_btc_outputs.sql
@@ -1,3 +1,7 @@
+{{ config(materialized='incremental',incremental_strategy='append')}}
+
+
+with flattened_outputs as (
 select
 tx.hash_key,
 tx.block_number,
@@ -11,3 +15,20 @@ from {{ ref('stg_btc')}} tx,
 LATERAL FLATTEN(input => outputs) f
 
 WHERE f.value:address is not null
+
+{% if is_incremental() %}
+
+and tx.block_timestamp >= (select max(block_timestamp) from {{ this }} )
+
+{% endif %}
+
+)
+select
+hash_key,
+block_number,
+block_timestamp,
+is_coinbase,
+output_address,
+output_value
+from flattened_outputs
+
